8b24798a0b9f5d9533ed81b4663292ff01905a72
rodrigo-medeiros <rodrigo.mangue@gmail.com>
2013-11-24 00:23:56 -0200

    Changed a variable name.

--- learn-you-the-nodejs-for-much-win/my-first-io.js
+++ learn-you-the-nodejs-for-much-win/my-first-io.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var fileBuffer = fs.readFileSync(process.argv[2], 'utf8');
-var numberOfLines = fileBuffer.split('\n').length - 1;
+var fileContents = fs.readFileSync(process.argv[2], 'utf8');
+var numberOfLines = fileContents.split('\n').length - 1;
 
 console.log(numberOfLines);
