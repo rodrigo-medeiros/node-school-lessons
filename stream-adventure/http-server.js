50825acb4bf3e7ee75299c41078ef5f9ba33f24b
rodrigo-medeiros <rodrigo.mangue@gmail.com>
2013-12-03 23:52:25 -0200

    Fixed a bug in the solution. Had to remove the name of the callback function, it worked when running on a Linux environment.

--- stream-adventure/http-server.js
+++ stream-adventure/http-server.js
@@ -3,7 +3,7 @@ var through = require('through');
 
 var server = http.createServer(function (req, res) {
   if (req.method === 'POST') {
-    req.pipe(through(function write (buf) {
+    req.pipe(through(function (buf) {
       this.queue(buf.toString().toUpperCase());
     })).pipe(res);
   }
