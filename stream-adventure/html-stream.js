48244179f3675bbe67f8b667366beb922f58aeb7
rodrigo-medeiros <rodrigo.mangue@gmail.com>
2013-12-04 00:11:08 -0200

    Added the solution to the HTML-Stream lesson.

---
+++ stream-adventure/html-stream.js
@@ -0,0 +1,11 @@
+var trumpet = require('trumpet');
+var through = require('through');
+var tr = trumpet();
+
+var stream = tr.select('.loud').createStream();
+
+stream.pipe(through(function (buf) {
+  this.queue(buf.toString().toUpperCase());
+})).pipe(stream);
+
+process.stdin.pipe(tr).pipe(process.stdout);
