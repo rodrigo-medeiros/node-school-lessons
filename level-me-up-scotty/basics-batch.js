bc6315fc70eb35cb546985078208c09716c8a68a
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2013-12-08 02:31:35 +0000

    Refactored the solution.

--- level-me-up-scotty/basics-batch.js
+++ level-me-up-scotty/basics-batch.js
@@ -1,26 +1,19 @@
-var level = require('level');
-var db = level(process.argv[2]);
-
-function batchOps (str) {
-  var op = str.split(',');
-
-  db.on('ready', function () {
-    if (op[0] === 'put')
-      db.batch().put(op[1], op[2]).write(function () {
-	console.error("put " + op[1] + " = " + op[2]);
-      });
-    else
-      db.batch().del(op[1]).write(function () {
-        console.error("delete " + op[1]);
-      });
-  });
-}
-
 var fs = require('fs');
-fs.readFile(process.argv[3], 'utf8', function (err, data) {
-  var content = data.split('\n');
-  content.forEach(function (item, index, arr) {
-    batchOps(item);
-  });
-});
+var level = require('level');
 
+var content = fs.readFileSync(process.argv[3], 'utf8').split('\n');
+
+level(process.argv[2], function (err, db) {
+  if (err)
+    throw err;
+
+  var batch = db.batch();
+  content.forEach(function (line) {
+    var op = line.split(',');
+    if (op[0] === 'put')
+      return batch.put(op[1],op[2]);
+    batch.del(op[1]);
+  });
+
+  batch.write();
+});
