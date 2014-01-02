e230c68c6282f9f4ea506c5e4c536f4adf74b947
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-01-02 01:12:39 -0200

    Added the solution to the 'Basic every some' lesson.

---
+++ functional-javascript/basic-every-some.js
@@ -0,0 +1,11 @@
+function checkUsersValid(goodUsers) {
+  return function (submittedUsers) {
+    return submittedUsers.every(function (submittedUser) {
+      return goodUsers.some(function (goodUser) {
+        return goodUser.id === submittedUser.id;
+      });
+    });
+  };
+}
+
+module.exports = checkUsersValid;
