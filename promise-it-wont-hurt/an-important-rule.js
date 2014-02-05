a973d6d880587a796d09862e3ba04baf1ac1023c
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-02-04 23:45:09 -0200

    Added the solution to the 'An important rule' lesson.

---
+++ promise-it-wont-hurt/an-important-rule.js
@@ -0,0 +1,23 @@
+var q = require('q');
+
+function throwMyGod() {
+  throw new Error("OH NOES");
+}
+
+function iterate(arg) {
+  console.log(arg);
+  return ++arg;
+}
+
+q.fcall(iterate, 1)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(throwMyGod)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(null, console.log);
