7dee357b48339e40a04e7b9f7402ec9aee2a0dc8
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-07-20 14:07:22 -0300

    Added the solution to the 'HANDLING' lesson.

---
+++ makemehapi/handling.js
@@ -0,0 +1,13 @@
+var Hapi = require("hapi");
+
+var server = Hapi.createServer("localhost", Number(process.argv[2] || 8080));
+
+server.route({
+  method: "GET",
+  path: "/",
+  handler: {
+    file: "index.html"
+  }
+});
+
+server.start();
