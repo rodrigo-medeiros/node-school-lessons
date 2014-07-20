2389866ec79641181195bcbee334f2be2425ef39
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-07-20 20:42:50 -0300

    Added the solution to the 'Directories' lesson.

---
+++ makemehapi/directories.js
@@ -0,0 +1,16 @@
+var Hapi = require('hapi'),
+    path = require('path');
+
+var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080));
+
+server.route({
+  method: 'GET',
+  path: '/foo/bar/baz/{filename}',
+  handler: {
+    directory: {
+      path: path.join(__dirname + '/public')
+    }
+  }
+});
+
+server.start();
