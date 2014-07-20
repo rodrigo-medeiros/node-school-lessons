31c9d07c2568690ef790a8ad7539c3f086565e62
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-07-20 13:38:52 -0300

    Added the default 8080 port.

--- makemehapi/routes.js
+++ makemehapi/routes.js
@@ -1,6 +1,6 @@
 var Hapi = require("hapi");
 
-var server = Hapi.createServer("localhost", Number(process.argv[2]));
+var server = Hapi.createServer("localhost", Number(process.argv[2] || 8080));
 
 server.route({
   method: 'GET',
