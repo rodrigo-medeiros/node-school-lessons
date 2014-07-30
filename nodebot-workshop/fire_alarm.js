770f9e879801e82c022441341b8a4f6635b658ab
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-07-30 00:24:30 -0300

    Almost ready, but still not working yet.

--- nodebot-workshop/fire_alarm.js
+++ nodebot-workshop/fire_alarm.js
@@ -2,9 +2,31 @@ var five = require('johnny-five'),
     board = new five.Board();
 
 board.on('ready', function () {
-  var sensor = new board.Sensor('A0'),
-      piezo = new board.Piezo(9),
-      led = new board.Led(13),
-      button = new board.Button(5);
+  var sensor = new five.Sensor('A0'),
+      piezo = new five.Piezo(9),
+      led = new five.Led(13),
+      button = new five.Button(5),
+      temperature = null,
+      isButtonPressed = false;
 
+  sensor.on('change', function () {
+    temperature = ((this.value * 0.004882814) - 0.5) * 100;
+    if (!isButtonPressed && temperature > 50) {
+      piezo.play({
+        song: [
+          ['C4', 1/4]
+        ]
+      });
+      led.strobe(0);
+    } else {
+      piezo.off();
+      led.stop().off();
+    }
+  });
 
+  button.on('press', function () {
+    isButtonPressed = true;
+    piezo.off();
+    led.stop().off();
+  });
+});
