4015a2a1716064cfe33adff5267f7d1236421ce6
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-07-31 00:47:10 -0300

    Now the solution to the 'Fire Alarm' lesson passed!

--- nodebot-workshop/fire_alarm.js
+++ nodebot-workshop/fire_alarm.js
@@ -7,25 +7,31 @@ board.on('ready', function () {
       led = new five.Led(13),
       button = new five.Button(5),
       temperature = null,
-      isButtonPressed = false;
+      wasButtonPressed = false;
 
   sensor.on('change', function () {
     temperature = ((this.value * 0.004882814) - 0.5) * 100;
-    if (!isButtonPressed && temperature > 50) {
-      piezo.play({
-        song: [
-          ['C4', 1/4]
-        ]
-      });
-      led.strobe(0);
+    if (temperature > 50) {
+      if (wasButtonPressed) {
+        piezo.off();
+        led.stop().off();
+      } else {
+        piezo.play({
+          song: [
+            ['C4', 1/4]
+          ]
+        });
+        led.strobe(0);
+      }
     } else {
+      wasButtonPressed = false;
       piezo.off();
       led.stop().off();
     }
   });
 
   button.on('press', function () {
-    isButtonPressed = true;
+    wasButtonPressed = true;
     piezo.off();
     led.stop().off();
   });
