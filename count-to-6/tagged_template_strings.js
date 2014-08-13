a393f9859fccece8db0d7681bbeb6d56f2b23f2a
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-08-13 00:28:15 -0300

    Added the partial solution to the 'Tagged Template Strings' lesson.

---
+++ count-to-6/tagged_template_strings.js
@@ -0,0 +1,6 @@
+console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
+
+function html(/* what goes here? */) {
+  // what goes here?
+  // don't forget to return the escaped string!
+}
