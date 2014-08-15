a0973c75aaf7e2dbe0a7f67409fa93e348370d68
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-08-15 00:33:52 -0300

    Added the partial solution to the 'Tagge Template Array' lesson.

--- count-to-6/tagged_template_strings.js
+++ count-to-6/tagged_template_strings.js
@@ -1,6 +1,17 @@
 console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
 
-function html(/* what goes here? */) {
-  // what goes here?
-  // don't forget to return the escaped string!
+function html (parts, ...arguments){
+  var html = parts[0];
+  for (var i = 0; i < arguments.length; i++) {
+    html += escape(arguments[i]) + parts[i + 1];
+  }
+  return html;
 }
+
+function escape (str) {
+  return str.replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/'/g, "&#39")
+            .replace(/'/g, "&quot");
+};
