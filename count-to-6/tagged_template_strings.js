537e6340e02e01df1beeacd5bcf993e7f5422d33
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-08-16 00:03:02 -0300

    Fixed the solution to the 'Tagged Template Strings' lesson

--- count-to-6/tagged_template_strings.js
+++ count-to-6/tagged_template_strings.js
@@ -1,17 +1,17 @@
 console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
 
-function html (parts, ...arguments){
+function html (parts, ...args){
   var html = parts[0];
-  for (var i = 0; i < arguments.length; i++) {
-    html += escape(arguments[i]) + parts[i + 1];
+  for (var i = 0; i < args.length; i++) {
+    html += escape(args[i]) + parts[i + 1];
   }
   return html;
-}
+};
 
 function escape (str) {
   return str.replace(/&/g, "&amp;")
             .replace(/</g, "&lt;")
             .replace(/>/g, "&gt;")
-            .replace(/'/g, "&#39")
-            .replace(/'/g, "&quot");
+            .replace(/'/g, "&#39;")
+            .replace(/'/g, "&quot;");
 };
