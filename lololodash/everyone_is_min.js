c790f9760ba407e5030aac024037972e89a56804
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-08-18 00:21:31 -0300

    Fixed the solution to the 'Everyone Is Min' lesson.

--- lololodash/everyone_is_min.js
+++ lololodash/everyone_is_min.js
@@ -1,15 +1,23 @@
 var _ = require('lodash');
 
 var worker = function (collection) {
-  var result = {};
-  result.hot = _.every(collection, function (item) {
-    if (item > 19) {
-      return item;
+  var result = {
+    hot: [],
+    warm: []
+  };
+
+  function isHot (collection) {
+    return collection > 19;
+  };
+
+  _.forEach(collection, function (town, townname) {
+    if (_.every(town, isHot)) {
+      result.hot.push(townname);
+    } else if (_.some(town, isHot)) {
+      result.warm.push(townname);
     }
   });
-  result.warm = _.some(collection, function (item) {
-    return item > 19;
-  });
+
   return result;
 };
 
