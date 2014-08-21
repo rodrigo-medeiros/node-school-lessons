827f6b8742a9cd1cea1c549bbee3672c07a18b76
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-08-21 15:26:04 -0300

    Fixed the solution to the 'Give Me An Overview' lesson.

--- lololodash/give_me_an_overview.js
+++ lololodash/give_me_an_overview.js
@@ -1,15 +1,29 @@
 var _ = require('lodash');
 
-var worker = function (collection) {
-  return _.reduce(
-    collection,
-    function (result, obj, key) {
-      result[key].article = obj.article;
-      result[key].total_articles = obj.quantity;
-      return result;
-    },
-    []
-  ).reverse();
+var worker = function (ordersList) {
+  var result = [];
+  ordersList = _.groupBy(ordersList, 'article');
+
+  _.forEach(ordersList, function (item, key) {
+    key = parseInt(key);
+    var total_orders = 0;
+
+    if (item.length === 1) {
+      total_orders = item[0].quantity;
+    } else {
+      total_orders = _.reduce(item, function (sum, item) {
+        return sum + item.quantity;
+      },
+      0);
+    }
+
+    result.push({
+      article: key,
+      total_quantity: total_orders
+    });
+  });
+
+  return _.sortBy(result, 'total_quantity').reverse();
 };
 
 module.exports = worker;
