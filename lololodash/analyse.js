377c80a149600ca803db1d51a1ba3a2daf7f80cc
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-08-23 00:11:29 -0300

    Still a partial solution. Not woking yet.

--- lololodash/analyse.js
+++ lololodash/analyse.js
@@ -1,7 +1,14 @@
 var _ = require('lodash');
 
 var worker = function (collection) {
-  
+  var result = {},
+      sum = _.reduce(
+        collection,
+        function (sum, item) {
+          return sum += item.income;
+        }, 0);
+  result['average'] = sum / collection.length;
+  return result;
 };
 
 module.exports = worker;
