26b6b631fb6b1027415e19f61c7ced575bf51c2e
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-08-23 16:27:22 -0300

    Fixed the solution.

--- lololodash/analyse.js
+++ lololodash/analyse.js
@@ -1,14 +1,29 @@
 var _ = require('lodash');
 
 var worker = function (collection) {
-  var result = {},
-      sum = _.reduce(
-        collection,
-        function (sum, item) {
-          return sum += item.income;
-        }, 0);
-  result['average'] = sum / collection.length;
-  return result;
+  var average,
+      underperform,
+      overperform;
+
+  collection = _.sortBy(collection, 'income');
+
+  average = _.reduce(collection, function (sum, item) {
+    return sum += item.income;
+  }, 0) / collection.length;
+
+  underperform = _.filter(collection, function (item) {
+    return item.income <= average;
+  });
+
+  overperform = _.filter(collection, function (item) {
+    return item.income > average;
+  });
+
+  return {
+    average: average,
+    underperform: underperform,
+    overperform: overperform
+  };
 };
 
 module.exports = worker;
