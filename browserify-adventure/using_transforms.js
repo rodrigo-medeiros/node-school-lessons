a4b51c753b81205934cd59f9458f9e5f66b5e206
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-08-29 00:18:39 -0300

    Changed the solution, but still not working.

--- browserify-adventure/using_transforms.js
+++ browserify-adventure/using_transforms.js
@@ -1,3 +1,5 @@
-var sprintf = require('sprintf');
+var sprintf = require('sprintf'),
+		fs      = require('fs');
 
-
+var text = fs.readFileSync('/home/rodrigomedeiros/local/lib/node_modules/browserify-adventure/problems/using_transforms/wake.txt', 'utf8');
+console.log(sprintf(text, "%3d %s"));
