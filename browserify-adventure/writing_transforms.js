516d3c7b82758392d6545da3d3da91721694c530
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-09-02 23:58:08 -0300

    Added a partial solution to the 'Writing Transforms' lesson.

---
+++ browserify-adventure/writing_transforms.js
@@ -0,0 +1,22 @@
+var through = require('through2'),
+    split = require('split'),
+    sprintf = require('sprintf');
+var quote = require('quote-stream');
+var combiner = require('stream-combiner');
+  
+module.exports = function (file) {
+    if (!/\.txt$/.test(file)) return through();
+    var num = 0;
+    var liner = through(function write (buf, enc, next) {
+        var line = buf.toString('utf8') + '\n';
+        if (num % 5 === 0) {
+            this.push(sprintf('%3d %s', num, line));
+        }
+        else this.push('    ' + line);
+        num ++;
+        next();
+    });
+    var prefix = through();
+    prefix.write('module.exports=');
+    return combiner(split(), liner, quote(), prefix);
+};
