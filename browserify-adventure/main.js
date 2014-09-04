44c25b3822859b92beaf1f2a1ba4318ba3040457
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-09-03 23:54:25 -0300

    Added the main.js file.

---
+++ browserify-adventure/main.js
@@ -0,0 +1,2 @@
+var txt = require('/home/rodrigomedeiros/local/lib/node_modules/browserify-adventure/problems/writing_transforms/wake.txt');
+console.log(txt);
--- browserify-adventure/writing_transforms.js
+++ browserify-adventure/writing_transforms.js
@@ -1,22 +1,24 @@
 var through = require('through2'),
     split = require('split'),
-    sprintf = require('sprintf');
-var quote = require('quote-stream');
-var combiner = require('stream-combiner');
+    sprintf = require('sprintf'),
+    quote = require('quote-stream'),
+    combiner = require('stream-combiner');
   
 module.exports = function (file) {
-    if (!/\.txt$/.test(file)) return through();
-    var num = 0;
-    var liner = through(function write (buf, enc, next) {
-        var line = buf.toString('utf8') + '\n';
-        if (num % 5 === 0) {
-            this.push(sprintf('%3d %s', num, line));
-        }
-        else this.push('    ' + line);
-        num ++;
-        next();
-    });
-    var prefix = through();
-    prefix.write('module.exports=');
-    return combiner(split(), liner, quote(), prefix);
+  if (!/\.txt$/.test(file)) return through();
+  
+  var num = 0;
+  var liner = through(function write (buf, enc, next) {
+    var line = buf.toString('utf8') + '\n';
+    if (num % 5 === 0) {
+      this.push(sprintf('%3d %s', num, line));
+    }
+    else this.push('    ' + line);
+    num ++;
+    next();
+  });
+
+  var prefix = through();
+  prefix.write('module.exports=');
+  return combiner(split(), liner, quote(), prefix);
 };
